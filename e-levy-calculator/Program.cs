202834c685a954168386f24be5cf181f4699ccf9
elijah amoako <elijahamoako72@gmail.com>
2022-07-05 14:16:03 +0000

    e-levy calculator

---
+++ e-levy-calculator/Program.cs
@@ -0,0 +1,21 @@
+﻿//Calculating how much a person will be charged if they send money above 100GHC.
+
+do
+{
+    double result = 0;
+    Console.Write("Enter a amount: ");
+    double amount = Convert.ToDouble(Console.ReadLine());
+
+    if (amount > 100)
+    {
+        result = amount * 0.015;
+        Console.WriteLine($"You will be charged: {result}GHC for sending {amount}GHC.");
+    }
+    else
+    {
+        Console.WriteLine($"You won't be charged for sending {amount}GHC.");
+    }
+    Console.WriteLine("Do you want to make another calculation? (Y = Yes, N = No)");
+
+} while (Console.ReadLine().ToUpper() == "Y");
+Console.WriteLine("Bye");
